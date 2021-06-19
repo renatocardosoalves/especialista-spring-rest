39233e8200b7a52a5f10bc3ab07bd94bbad4cec1
Renato Cardoso Alves <renatocardoso.dev@gmail.com>
2021-06-19 15:53:04 -0300

    Dependência opcional com @Autowired

--- algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-@Component
+// @Component
 public class NotificadorEmail implements Notificador {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NotificadorEmail.class);
--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -2,32 +2,27 @@ package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
-//    @Autowired
-    private final Notificador notificador;
+    private static final Logger LOGGER = LoggerFactory.getLogger(AtivacaoClienteService.class);
 
-//    @Autowired
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-    }
-
-//    public AtivacaoClienteService(String qualquer) {
-//
-//    }
+    @Autowired(required = false)
+    private Notificador notificador;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-    }
 
-//    @Autowired
-//    public void setNotificador(Notificador notificador) {
-//        this.notificador = notificador;
-//    }
+        if (notificador != null) {
+            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        } else {
+            LOGGER.info(">>> Não existe notificador, mas o cliente foi ativado");
+        }
+    }
 
 }
