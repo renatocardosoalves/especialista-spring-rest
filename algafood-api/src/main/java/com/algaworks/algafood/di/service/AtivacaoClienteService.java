9042635d14302ad00bca87086dc02fb3047844d8
Renato Cardoso Alves <renatocardoso.dev@gmail.com>
2021-06-20 00:23:11 -0300

    Criando métodos de callback do ciclo de vida dos beans

--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -4,15 +4,31 @@ import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.NivelUrgencia;
 import com.algaworks.algafood.di.notificacao.Notificador;
 import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+//@Component
 public class AtivacaoClienteService {
 
-    private final Notificador notificador;
+    private static final Logger LOGGER = LoggerFactory.getLogger(AtivacaoClienteService.class);
 
-    public AtivacaoClienteService(@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA) Notificador notificador) {
-        this.notificador = notificador;
+    @Autowired
+    @TipoDoNotificador(NivelUrgencia.URGENTE)
+    private Notificador notificador;
+
+//    @PostConstruct
+    public void init() {
+        LOGGER.info(">>> init() {}", notificador);
+    }
+
+//    @PreDestroy
+    public void destroy() {
+        LOGGER.info(">>> destroy()");
     }
 
     public void ativar(Cliente cliente) {
