34a9741a0037ab7d21e0e410ef5db42fbe934073
Renato Cardoso Alves <renatocardoso.dev@gmail.com>
2021-06-20 04:18:42 -0300

    Criando e acessando propriedades customizadas com @Value

--- algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.di.notificacao;
 import com.algaworks.algafood.di.modelo.Cliente;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @TipoDoNotificador(NivelUrgencia.URGENTE)
@@ -11,9 +12,17 @@ public class NotificadorEmail implements Notificador {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NotificadorEmail.class);
 
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+    
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
+    
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        LOGGER.info(">>> Notificando {} através do e-mail {}: {}",
+        LOGGER.info("Host: {} Porta: {}", host, porta);
+    	
+    	LOGGER.info(">>> Notificando {} através do e-mail {}: {}",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
 
