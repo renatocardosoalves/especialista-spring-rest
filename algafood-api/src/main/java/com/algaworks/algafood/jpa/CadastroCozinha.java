040dbe0dd0af52acfa53bacfac5de0110b7c4ba7
Renato Cardoso Alves <renatocardoso.dev@gmail.com>
2021-06-20 23:09:57 -0300

    Adicionando um objeto no banco de dados

--- algafood-api/src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.jpa;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -16,6 +17,11 @@ public class CadastroCozinha {
     public List<Cozinha> lista() {
         return manager.createQuery("from Cozinha", Cozinha.class)
                 .getResultList();
+    }
+
+    @Transactional
+    public Cozinha adicionar(Cozinha cozinha) {
+        return manager.merge(cozinha);
     }
 
 }
---
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/InclusaoCozinhaMain.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafood.jpa;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.List;
+
+public class InclusaoCozinhaMain {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InclusaoCozinhaMain.class);
+
+    public static void main(String[] args) {
+        var applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        var cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        var cozinha1 = new Cozinha();
+        cozinha1.setNome("Brasileira");
+
+        var cozinha2 = new Cozinha();
+        cozinha2.setNome("Japonesa");
+
+        cozinha1 = cadastroCozinha.adicionar(cozinha1);
+        LOGGER.info(">>> id: {}, nome: {}", cozinha1.getId(), cozinha1.getNome());
+
+        cozinha2 = cadastroCozinha.adicionar(cozinha2);
+        LOGGER.info(">>> id: {}, nome: {}", cozinha2.getId(), cozinha2.getNome());
+    }
+
+}
