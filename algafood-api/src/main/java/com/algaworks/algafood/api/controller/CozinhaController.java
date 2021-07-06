762326cd38de5ddccda43e49681e092402b7fb0f
Renato Cardoso Alves <renatocardoso.dev@gmail.com>
2021-07-05 21:51:13 -0300

    Modelando e requisitando um Collection Resource com GET

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    private final CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+}
