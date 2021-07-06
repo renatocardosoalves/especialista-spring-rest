2b17a6ea41c9b4fc62c60f29b2580c3fb7aecae1
Renato Cardoso Alves <renatocardoso.dev@gmail.com>
2021-07-05 21:54:34 -0300

    Desafio: collection resource de estados

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+    private final EstadoRepository estadoRepository;
+
+    @GetMapping
+    public List<Estado> listar() {
+        return estadoRepository.listar();
+    }
+}
