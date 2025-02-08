874620b6d0cd68dc27c7ec4e0015cd54fbe0a589
Renato Cardoso Alves <rca.develop@gmail.com>
2025-02-08 01:24:34 -0300

    exemplo de uso do spring profiles

--- algafood-api/src/main/java/br/com/alura/algafoodapi/service/NotificadorEmailMock.java
+++ algafood-api/src/main/java/br/com/alura/algafoodapi/service/NotificadorEmailMock.java
@@ -5,16 +5,16 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @TipoDoNotificador(NivelUrgencia.NORMAL)
-@Profile("prod")
+@Profile("dev")
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorEmailMock implements Notificador {
 
-    public NotificadorEmail() {
-        System.out.println("NotificadorEmail");
+    public NotificadorEmailMock() {
+        System.out.println("NotificadorEmailMock");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
+        System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
