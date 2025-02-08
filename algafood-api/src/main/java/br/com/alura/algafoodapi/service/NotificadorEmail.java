a1ad12651863f81cd0f6ab5d5dfdbb8bfd1fa3c4
Renato Cardoso Alves <rca.develop@gmail.com>
2025-02-08 13:11:17 -0300

    exemplo de uso do configuration properties para acrepresentar configurações do arquivo de configuração do spring boot

--- algafood-api/src/main/java/br/com/alura/algafoodapi/service/NotificadorEmail.java
+++ algafood-api/src/main/java/br/com/alura/algafoodapi/service/NotificadorEmail.java
@@ -7,8 +7,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
+    private final NotificadorProperties notificadorProperties;
+
+    public NotificadorEmail(NotificadorProperties notificadorProperties) {
+        this.notificadorProperties = notificadorProperties;
+    }
+
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + notificadorProperties.getHostServidor() + " Porta: " + notificadorProperties.getPortaServidor());
         System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
