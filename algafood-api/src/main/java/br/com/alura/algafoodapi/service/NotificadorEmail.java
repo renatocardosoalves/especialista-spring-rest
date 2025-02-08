68b131dc09c7ec7d2a403359613a3175ce0dbf8a
Renato Cardoso Alves <rca.develop@gmail.com>
2025-02-08 13:00:01 -0300

    exemplo de uso do value para acessar propriedades do arquivo de configuração do spring boot

--- algafood-api/src/main/java/br/com/alura/algafoodapi/service/NotificadorEmail.java
+++ algafood-api/src/main/java/br/com/alura/algafoodapi/service/NotificadorEmail.java
@@ -1,14 +1,22 @@
 package br.com.alura.algafoodapi.service;
 
 import br.com.alura.algafoodapi.model.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
 
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
+
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + host + " Porta: " + porta);
         System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
     }
 }
